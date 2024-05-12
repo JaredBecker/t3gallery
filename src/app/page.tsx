239b7a7653dc9ce8d71c2b38a0fe1465a1b73aea
Jared-Becker <jared@warpdevelopment.com>
2024-05-12 22:42:14 +0200

    Linking the database to the UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,30 +5,16 @@ import { db } from "~/server/db";
 // This tells Next.js not to cache this route which will result in this route being rendered for each user at request time
 export const dynamic = "force-dynamic";
 
-// Uploaded via uploadthing.com
-const mockUrls = [
-  "https://utfs.io/f/3fe8e8da-95e2-49d0-bfb4-4cbc98b80a3b-5pd0us.jpg",
-  "https://utfs.io/f/ac39f599-ad97-49f6-a471-fdd3573d1b26-qnj95w.jpg",
-];
-
-const mockImages = mockUrls.map((url, i) => ({
-  id: i + 1,
-  url,
-}));
-
 export default async function HomePage() {
   // Make sure to import from ~/server/db to use our own database instance and not from @vercel/postgres
-  const posts = await db.query.posts.findMany();
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
 
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (
-          <div key={post.id} className="w-48">
-            <p className="text-2xl">{post.name}</p>
-          </div>
-        ))}
-        {[...mockImages, ...mockImages, ...mockImages].map((img, i) => (
+        {[...images, ...images, ...images].map((img, i) => (
           <div key={img.id + "-" + i} className="w-48">
             <img src={img.url} />
           </div>
