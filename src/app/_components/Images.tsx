0202e3e91ab3703f8ace2027384ce180ccfbe728
Jared-Becker <jared@warpdevelopment.com>
2024-05-13 02:23:42 +0200

    Break queries into seperate file

--- src/app/_components/Images.tsx
+++ src/app/_components/Images.tsx
@@ -1,10 +1,7 @@
-import { db } from "~/server/db";
+import { getMyImages } from "~/server/queries";
 
 export default async function Images() {
-  // Make sure to import from ~/server/db to use our own database instance and not from @vercel/postgres
-  const images = await db.query.images.findMany({
-    orderBy: (model, { desc }) => desc(model.id),
-  });
+  const images = await getMyImages();
 
   return (
     <div className="flex flex-wrap gap-4">
--- src/server/queries.ts
+++ src/server/queries.ts
@@ -1,2 +1,21 @@
 // This package makes sure that the code in this file is only run on the server
 import "server-only";
+import { db } from "./db";
+import { auth } from "@clerk/nextjs/server";
+
+export async function getMyImages() {
+  const user = auth();
+
+  if (!user.userId) throw new Error("Unauthorized");
+
+  // Make sure to import from ~/server/db to use our own database instance and not from @vercel/postgres
+  const images = await db.query.images.findMany({
+    where: (model, { eq }) => eq(model.userId, user.userId),
+    orderBy: (model, { desc }) => desc(model.id),
+  });
+
+  // You can use next taint here to prevent the client from seeing info from the server that they shouldn't see like
+  // passwords or other sensitive data.
+
+  return images;
+}
